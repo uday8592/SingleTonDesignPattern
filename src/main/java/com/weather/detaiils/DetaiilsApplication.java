759d6d6f1560e48f3a6f69e3db5b7ffa3f4c9ac0
uday8592 <135016621+uday8592@users.noreply.github.com>
2026-01-29 19:02:45 +0530

    first Commit

---
+++ src/main/java/com/weather/detaiils/DetaiilsApplication.java
@@ -0,0 +1,13 @@
+package com.weather.detaiils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetaiilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetaiilsApplication.class, args);
+	}
+
+}
